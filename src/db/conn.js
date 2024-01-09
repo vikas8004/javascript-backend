bf872043c6c826a10ae483df6783280874bfc8a4
vikas <vikas80046@gmail.com>
2024-01-09 11:53:40 +0530

    woriking on utils

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     const connectionReference = await mongoose.connect(process.env.MONGO_URI);
-    console.log(connectionReference.connection.host);
+    console.log("host : ",connectionReference.connection.host);
     console.log("connection to database is successful");
   } catch (error) {
     console.log("error from mongoDbConnection", error);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
 import dotenv from "dotenv";
-dotenv.config()
-import connectDb from "./db/conn.js"
+dotenv.config();
+import express from "express";
+import connectDb from "./db/conn.js";
+import { app } from "./app.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
 
+app.use(cookieParser());
 connectDb()
+  .then((res) => {
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+      console.log(
+        `ur applicaton is running on the port number ${process.env.PORT}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log("mongo db connection failed ", err.message);
+  });
